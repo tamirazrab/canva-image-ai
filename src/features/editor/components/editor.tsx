c2282f99b419c454efecfa6951a3c903eeed9eb8
t.azrab <tamirazrab@gmail.com>
2024-09-01 16:38:52 +0500

    feat: :sparkles: add basic canvas

---
+++ src/features/editor/components/editor.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEditor } from "@/features/editor/hooks/use-editor";
+import { useEffect, useRef } from "react";
+import { fabric } from "fabric";
+
+export const Editor = () => {
+  const { init } = useEditor();
+
+  const canvasRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const canvas = new fabric.Canvas(canvasRef.current!, {
+      controlsAboveOverlay: true,
+      preserveObjectStacking: true,
+    });
+
+    init({
+      initialCanvas: canvas,
+      initialContainer: containerRef.current!,
+    });
+  }, [init]);
+
+  return (
+    <div className="flex h-full flex-col">
+      <div className="flex-1 h-full bg-muted" ref={containerRef}>
+        <canvas ref={canvasRef}></canvas>
+      </div>
+    </div>
+  );
+}
